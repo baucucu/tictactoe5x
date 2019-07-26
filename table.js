e05ed035a7c081d66a8adfadf111de1af52129d4
Alex Raduca <alexandru.raduca@gmail.com>
2019-07-26 10:25:28 +0300

    Add files via upload
    replaced var with let

--- table.js
+++ table.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-    var number_of_rows = 15
-    var number_of_cols = 15
+    let number_of_rows = 15
+    let number_of_cols = 15
     
-    var table_body;
+    let table_body;
     
-    for(var i=0;i<number_of_rows;i++){
+    for(let i=0;i<number_of_rows;i++){
     table_body+='<tr>';
-    for(var j=0;j<number_of_cols;j++){
+    for(let j=0;j<number_of_cols;j++){
         table_body += `<td><button class="tile" id="${i}_${j}"></button></td>`
     }
     table_body+='</tr>';
